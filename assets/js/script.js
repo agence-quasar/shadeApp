97e822ba92a8ef3f8ee5d10a169f6435a2472e0b
amb <ambfrom91@gmail.com>
2017-06-10 13:02:43 +0200

    f

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $('.scrollTo').on('click', function() { // Au clic sur un élément
-        var page = $(this).attr('href'); // Page cible
-        var speed = 800; // Durée de l'animation (en ms)
-        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
-        return false;
-    });
-});
