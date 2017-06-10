f4030bf2bda0fcd01f3f1537bfd309299ac255cf
amb <ambfrom91@gmail.com>
2017-06-10 16:45:55 +0200

    start firebase

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,36 @@
 $(document).ready(function() {
     $('.scrollTo').on('click', function() { // Au clic sur un élément
-        var page = $(this).attr('href'); // Page cible
-        var speed = 800; // Durée de l'animation (en ms)
+        let page = $(this).attr('href'); // Page cible
+        let speed = 800; // Durée de l'animation (en ms)
         $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
         return false;
     });
+
+
+
+    // Initialize Firebase
+  const config = {
+    apiKey: "AIzaSyBHhngkXqUoSwm0ilnCbY0gXUFlmI9vOy4",
+    authDomain: "shadeapp-c4b0e.firebaseapp.com",
+    databaseURL: "https://shadeapp-c4b0e.firebaseio.com",
+    projectId: "shadeapp-c4b0e",
+    storageBucket: "shadeapp-c4b0e.appspot.com",
+    messagingSenderId: "56916114464"
+  };
+  firebase.initializeApp(config);
 });
+	
+
+   let database = firebase.database()
+   dataTest = " coicou ceci est un est "
+
+document.getElementById('download-button').addEventListener('click',function(myData){
+
+	firebase.database().ref('data/' + myData).set({
+	    myData : dataTest
+	  });
+
+});
+  
+
+
