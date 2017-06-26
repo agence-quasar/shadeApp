e97f2d5d552b598bf92330031f0f9304c34c9104
Arnaman <arnaud1.mathieu@gmail.com>
2017-06-26 16:29:20 +0200

    Adaptation layout sur mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
     $(document).ready(function() {
         $('.js-scrollTo').on('click', function() { // Au clic sur un élément
             var page = $(this).attr('href'); // Page cible
-            var speed = 750; // Durée de l'animation (en ms)
+            var speed = 1000; // Durée de l'animation (en ms)
             $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
             return false;
         });
